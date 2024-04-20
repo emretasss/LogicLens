b81ecc8ac920e1b654b8d75ef88d9135da9466d7
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-21 02:17:55 +0300

    "Added comments to dashboard page component"
    The code diff includes the addition of several empty lines and comments in the `DashboardPage` component. There are also comments added at the top of the file with a date and a possible TODO

--- logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@/components/ui/button";
 
+
+
+//dashboard  21.04.24 2.16 go authentication maybe clerk
 const DashboardPage = () => {
   return (
     <p>Dashboard Page   (Proteced)</p>
