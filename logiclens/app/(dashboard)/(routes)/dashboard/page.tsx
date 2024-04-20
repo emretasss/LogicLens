c0cd1642095746ab9946071b1e98bc6ec6d58aaa
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-21 01:40:49 +0300

    "Add destructive variant Button component to DashboardPage"
    This commit adds a new `Button` component with the `destructive` variant to the `DashboardPage` component.

---
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <Button variant="destructive">clickk</Button>
+
+    );
+}
+export default DashboardPage ;
