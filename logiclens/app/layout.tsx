326740a364d9d7752f5d59b6df91a564cc1e7009
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-21 14:54:43 +0300

    providercmpnt clerk

--- logiclens/app/layout.tsx
+++ logiclens/app/layout.tsx
@@ -16,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
+
+
