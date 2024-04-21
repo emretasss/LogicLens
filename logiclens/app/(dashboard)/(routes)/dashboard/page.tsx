c9072b09ad1ac4f26fb510861624428a0c44e009
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-21 21:54:52 +0300

    Update DashboardPage component; remove comment about moving to authentication
    This commit updates the DashboardPage component by removing a comment that suggested moving to authentication. The component still contains a heading indicating that it is a protected page.

--- logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button";
 
 
 
-//dashboard  21.04.24 12.45 go authentication maybe clerk
 const DashboardPage = () => {
   return (
     <p>Dashboard Page   (Proteced)</p>
