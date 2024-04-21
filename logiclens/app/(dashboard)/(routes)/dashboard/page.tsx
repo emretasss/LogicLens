c277f58cb7b6df086d691c17cce922e9d43b643a
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-21 12:45:22 +0300

    12.45

--- logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 
 
-//dashboard  21.04.24 2.16 go authentication maybe clerk
+//dashboard  21.04.24 12.45 go authentication maybe clerk
 const DashboardPage = () => {
   return (
     <p>Dashboard Page   (Proteced)</p>
