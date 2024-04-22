499174aad187f429a6f8248b5c26f35f315de5e5
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-23 02:14:11 +0300

    Update DashboardLayout to add a hidden fixed sidebar on medium screens and above
    The changes include:
    
    * Wrapping the existing layout in a new div with a class name of "h-full relative".
    * Adding a

--- logiclens/app/(dashboard)/layout.tsx
+++ logiclens/app/(dashboard)/layout.tsx
@@ -6,8 +6,12 @@ const DashboardLayout = (
 
 ) => {
     return (
-    <div >
-            {children}
+    <div className="h-full relative">
+        <div className="hidden h-full md:flex 
+         md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900
+        "> 
+
+        </div>
 
     </div> );
 }
