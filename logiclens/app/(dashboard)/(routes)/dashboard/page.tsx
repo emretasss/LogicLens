74a0863105163fe864a702a7ee791569d0fea259
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-23 00:46:36 +0300

    Update Dashboard page by adding UserButton component
    This commit updates the Dashboard page by adding the UserButton component from '@clerk/nextjs'. No other changes were made to the code.

--- logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,12 @@
 import { UserButton } from '@clerk/nextjs';
 
-
+//00.46 23.05.24
 
 const DashboardPage = () => {
   return (
     <div>
     <p>Dashboard Page </p>
-    
+
     <UserButton/>
     </div>
     );
