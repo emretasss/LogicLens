f4826a373f9a4c7b8e65e24ecdee44e67beb47d1
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-22 22:55:08 +0300

    authlayout

---
+++ logiclens/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+const AuthLayout = ({
+    children
+}: {
+    children :React.ReactNode;
+
+}
+
+) => {
+    return (
+        <div>
+            {children}
+        </div>
+      );
+}
+ 
+export default AuthLayout;
