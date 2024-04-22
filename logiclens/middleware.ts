73e04cef1d83e521d0b1e3ae590b2a7f3581e4df
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-22 21:48:56 +0300

    Update logiclens/middleware.ts: Add authMiddleware to check if user is authenticated
    This update adds a new middleware function to check if a user is authenticated or not. If the user is authenticated

--- logiclens/middleware.ts
+++ logiclens/middleware.ts
@@ -1,5 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+
+//middle function to check if user is authenticated or not. If the user is authenticated, it will allow them to access the page otherwise redirect them 
 export default authMiddleware({
   publicRoutes: ["/"],
 });
