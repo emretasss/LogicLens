981f19b4b8a252fe65f20ce3e1a61f2c47e840c2
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-22 23:31:30 +0300

    Add Next Link component to LogiClens landing page
    This commit adds the Next Link component to the LogiClens landing page, allowing for navigation to the dashboard page. Additionally, the package-lock.json file has been updated

--- logiclens/app/(landing)/page.tsx
+++ logiclens/app/(landing)/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const landing = () => {
     return (
          <div>
-                Landing Page (unprotected)
-            
-            </div>
+                Landing Page (unprotected)  
+                
+                <Link  href="/dashboard"> 
+               
+                </Link>
+             
+              </div>
           );
 }
  
