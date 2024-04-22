3353bbdea7cab067cfcb77300359c7b9c0ce7834
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-22 13:32:17 +0300

    - Remove: publishableKey attribute with process.env.NEXT\_PUBLIC\_CLERK\_PUBLISHABLE\_KEY - Add: empty ClerkProvider (using default

--- logiclens/app/layout.tsx
+++ logiclens/app/layout.tsx
@@ -16,10 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider 
-    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    
-    >
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
