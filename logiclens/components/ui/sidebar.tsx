c31c8dc88da51d251468d168f836e7d7430fd3ab
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-23 03:21:31 +0300

    Update sidebar component with basic layout
    The changes include:
    
    * Adding a Sidebar functional component
    * Defining the basic layout and style for the sidebar using Tailwind CSS classes
    * Exporting the Sidebar

--- logiclens/components/ui/sidebar.tsx
+++ logiclens/components/ui/sidebar.tsx
@@ -1,1 +1,14 @@
 "use client";
+
+const Sidebar = () => {
+    return ( 
+
+        <div className="space-y-4 py-4 flex flex-col h-full
+        bg-[#111827] text-white">
+            sidebar component
+
+        </div>
+     );
+}
+ 
+export default Sidebar;
