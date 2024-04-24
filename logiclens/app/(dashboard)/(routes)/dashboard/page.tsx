d7032fa93cf0c4e63f9c009ff48b6b38acaf4417
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-24 17:48:10 +0300

    Update DashboardPage component with new text and layout
    The diff includes:
    
    * Adding new text to the h1 and p elements within the DashboardPage component
    * Wrapping the existing content in a new div with a class

--- logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,19 @@
 
 
 const DashboardPage = () => {
+
+  
+
+
   return (
     <div  className="mb-8 space-y-4">
-    <h1 className="text-2xl md:text-4xl font-bold tet-center">Explore the powe of AI</h1>
-   
-   
+    <h1 className="text-2xl md:text-4xl font-bold text-center">Explore the powe of AI</h1>
+    <p className="text-muted-foreground font-ligt   text-sm md:text-lg text-center ">
+      Chat with the smartest AI -Experience the power of AI
+    </p>
+    <div className="px-4 md:px-20  lg:px-32 spacce-y-4">
+
+    </div>
     </div>
     );
 }
