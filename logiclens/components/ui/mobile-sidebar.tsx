957a410a3fede496f14fe68b18baa74cd39e1a70
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-24 04:26:53 +0300

    mobilesidebar

---
+++ logiclens/components/ui/mobile-sidebar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { Menu } from 'lucide-react';
+
+
+const MobileSidebar = () => {
+    return ( 
+
+        <Button   >
+            <Menu />
+        </Button>
+
+     );
+}
+ 
+export default MobileSidebar;
