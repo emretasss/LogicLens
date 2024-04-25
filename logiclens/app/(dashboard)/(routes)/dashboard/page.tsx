6c6def51caef19b128ea6996058e4fc6ab219347
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-25 03:42:06 +0300

    Update colors for Image Generation tool in dashboard page
    Change the text color of "Image Generation" tool from "text-pink-500" to "text-pink-700" and background color from "bg

--- logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -23,8 +23,8 @@ const tools=[
   {
     label:" Image Generation",
     icon:Image,
-    color:" text-pink-500 ",
-    bgColor:"bg-pink-500/100",
+    color:" text-pink-700 ",
+    bgColor:"bg-pink-700/100",
     href:"/conversation"
   },
   {
