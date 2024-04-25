8e15670c571d5a2050bf40db34f9225d8feb36cf
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-25 03:30:27 +0300

    "Changed tool icon background to foreground color and removed size prop in tool.icon"

--- logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -37,9 +37,9 @@ const DashboardPage = () => {
         className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
         > 
         <div className="flex items-center gap-x-4">
-        <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+        <div className={cn("p-2 w-fit rounded-md", tool.color)}>
 
-        <tool.icon className={cn("w-8 h-8 ",tool.color)} />
+        <tool.icon  />
         </div>
         </div>
 
