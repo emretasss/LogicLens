7646ae710405134c6ea00d8b727cfe31bd058d3c
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-25 03:18:41 +0300

    Add cn function to DashboardPage and modify tool card layout
    The diff adds the `cn` function from `@/lib/utils` to the `DashboardPage` component and modifies the layout of the tool card by moving

--- logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import { MessageSquare } from "lucide-react";
 
 
@@ -35,8 +36,11 @@ const DashboardPage = () => {
         <Card key={tool.href}  
         className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
         > 
-        <div className="flex items-center">
+        <div className="flex items-center gap-x-4">
+        <div className={cn("",)}>
 
+
+        </div>
         </div>
 
 
