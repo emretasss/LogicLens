14a4ced525fc7e14c8ed1e8d77f440223eb4abea
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-27 03:35:42 +0300

    "Update Header component: add Icon wrapper for header icon"
    This commit updates the Header component in the logiclens/components/header.tsx file. It adds a new `div` wrapper for the header icon, allowing for easier

--- logiclens/components/header.tsx
+++ logiclens/components/header.tsx
@@ -26,8 +26,9 @@ export const Heading = ( {
         
      <div  className="px-4 lg:px-8 flex items-center gap-x-3 mb-8" >
         <div className={cn(" p-2 w-fit rounded-md ",bgColor)}>
+            <Icon className={cn("w-10 h-10", iconColor)}/>
 
-
+            
             </div>            
     </div> 
 
