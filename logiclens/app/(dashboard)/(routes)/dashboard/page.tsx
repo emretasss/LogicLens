15fbad4e5c5b0824e664343f9869a9fdfe2522d8
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-04-29 01:09:35 +0300

    This commit corrects a spelling mistake in the header of the dashboard page. The word "powe" is changed to "power" in the sentence "Ex

--- logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
+++ logiclens/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -55,7 +55,7 @@ const router =useRouter();
       
 
     <div  className="mb-8 space-y-4">
-    <h1 className="text-2xl md:text-4xl font-bold text-center">Explore the powe of AI</h1>
+    <h1 className="text-2xl md:text-4xl font-bold text-center">Explore the power of AI</h1>
     <p className="text-muted-foreground font-ligt   text-sm md:text-lg text-center ">
       Chat with the smartest AI -Experience the power of AI
     </p>
